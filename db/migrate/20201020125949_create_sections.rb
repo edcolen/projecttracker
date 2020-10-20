fbd1bb58e991e9f7b133836393c9eaf62b5ef9e0
Ed <ed.colen@gmail.com>
2020-10-20 11:54:34 -0300

    updated section model migration

--- db/migrate/20201020125949_create_sections.rb
+++ db/migrate/20201020125949_create_sections.rb
@@ -1,7 +1,14 @@
 class CreateSections < ActiveRecord::Migration[6.0]
   def change
     create_table :sections do |t|
-
+      t.references :project, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :leader, foreign_key: { to_table: 'users' }
+      t.references :color, foreign_key: true
+      t.string :title, null: false
+      t.string :description
+      t.datetime :start_date
+      t.datetime :end_date
       t.timestamps
     end
   end
