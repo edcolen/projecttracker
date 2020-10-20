0864345d1821ae101e0c7759e600f4b69fdc0c40
Ed <ed.colen@gmail.com>
2020-10-20 15:33:01 -0300

    updated card model migration

--- db/migrate/20201020130013_create_cards.rb
+++ db/migrate/20201020130013_create_cards.rb
@@ -1,7 +1,17 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-
+      t.references :user, foreign_key: true
+      t.references :section, foreign_key: true
+      t.references :leader, foreign_key: { to_table: 'users' }
+      t.references :color, foreign_key: true
+      t.string :title, null: false
+      t.string :description
+      t.string :priority
+      t.string :status
+      t.string :category
+      t.datetime :start_date
+      t.datetime :end_date
       t.timestamps
     end
   end
