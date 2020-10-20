edea4d0924ee08fde7fc273be2c34f1cc7766793
Ed <ed.colen@gmail.com>
2020-10-20 11:22:51 -0300

    updated project migration

--- db/migrate/20201020125918_create_projects.rb
+++ db/migrate/20201020125918_create_projects.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-
+      t.references :user, foreign_key: true
+      t.string :title, null: false
+      t.string :description
+      t.datetime :start_date
+      t.datetime :end_date
       t.timestamps
     end
   end
