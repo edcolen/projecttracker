eafac6ce838bf989a3dc1e5bb1c3598be627d751
Ed <ed.colen@gmail.com>
2020-10-20 20:06:56 -0300

    updated response migration

--- db/migrate/20201020225755_create_responses.rb
+++ db/migrate/20201020225755_create_responses.rb
@@ -1,7 +1,9 @@
 class CreateResponses < ActiveRecord::Migration[6.0]
   def change
     create_table :responses do |t|
-
+      t.references :user, foreign_key: true
+      t.references :comment, foreign_key: true
+      t.text :content
       t.timestamps
     end
   end
