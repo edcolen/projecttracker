8d6044f2f4c6d62dc750071eb498b511aec0fd14
Ed <ed.colen@gmail.com>
2020-10-20 19:59:25 -0300

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_130914) do
+ActiveRecord::Schema.define(version: 2020_10_20_225755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,11 @@ ActiveRecord::Schema.define(version: 2020_10_20_130914) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "section_taggings", force: :cascade do |t|
