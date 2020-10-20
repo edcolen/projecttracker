52427bacca30550e2ce7620cb1f9fe57b7ae9597
Ed <ed.colen@gmail.com>
2020-10-20 16:26:32 -0300

    updated collaboration model migration

--- db/migrate/20201020130727_create_collaborations.rb
+++ db/migrate/20201020130727_create_collaborations.rb
@@ -1,7 +1,10 @@
 class CreateCollaborations < ActiveRecord::Migration[6.0]
   def change
     create_table :collaborations do |t|
-
+      t.references :user, foreign_key: true
+      t.references :project, foreign_key: true
+      t.boolean :member, default: true
+      t.string :role
       t.timestamps
     end
   end
