eba0bd8103e6f74fb84d14b50a074eb09e7a6b49
Ed <ed.colen@gmail.com>
2020-10-20 15:58:21 -0300

    updated notification model migration

--- db/migrate/20201020130107_create_notifications.rb
+++ db/migrate/20201020130107_create_notifications.rb
@@ -1,7 +1,9 @@
 class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
-
+      t.references :user, foreign_key: true
+      t.string :notification_action
+      t.string :notification_target
       t.timestamps
     end
   end
