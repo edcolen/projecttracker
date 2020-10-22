a1f5c9704466900434003afd5378ec6f2c804b0e
Ed <ed.colen@gmail.com>
2020-10-21 21:43:02 -0300

    added project relations and validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  has_many :sections
+  has_many :cards, through: :sections
+  has_many :tags, through: :section_taggings
+  has_many :collaborations, class_name: 'Collaboration', foreign_key: 'project_id'
+
+  # Validations
+  validates :title, presence: true
 end
