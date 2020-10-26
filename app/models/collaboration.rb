96917b7798ed4811522d1ec908af2001400bbc02
Ed <ed.colen@gmail.com>
2020-10-26 09:24:35 -0300

    updated relations

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,5 +1,5 @@
 class Collaboration < ApplicationRecord
   belongs_to :user
   belongs_to :member, class_name: 'User'
-  belongs_to :project, class_name: 'Project'
+  belongs_to :project
 end
--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,5 +1,5 @@
 class Privilege < ApplicationRecord
   belongs_to :user
   belongs_to :admin, class_name: 'User'
-  belongs_to :project, class_name: 'Project'
+  belongs_to :project
 end
