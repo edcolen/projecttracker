aef07f9cac3bf83480a6a3af0b7dbf5d67f79b88
Ed <ed.colen@gmail.com>
2020-10-26 09:39:26 -0300

    created card relations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ApplicationRecord
+  belongs_to :user
+  belongs_to :section
+  belongs_to :leader, class_name: 'User'
+  belongs_to :color
+
+  has_many :comments
+  has_many :card_taggings
+  has_many :card_teamings
+
+  # Validations
+  validates :title, :status, presence: true
 end
