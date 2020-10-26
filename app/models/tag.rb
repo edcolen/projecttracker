285bb70f8761c6dfd2b3038d40ab71a77fdb53f0
Ed <ed.colen@gmail.com>
2020-10-26 09:45:42 -0300

    updated tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,8 @@
 class Tag < ApplicationRecord
-  validates :name, presence: true
-
   has_many :section_taggings
   has_many :card_taggings
   has_many :comment_taggings
+
+  # Validations
+  validates :name, presence: true
 end
