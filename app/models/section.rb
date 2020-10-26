864efe58124c41b296826ea370b4b739191d75c4
Ed <ed.colen@gmail.com>
2020-10-26 09:24:26 -0300

    created section relations

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,13 @@
 class Section < ApplicationRecord
+  belongs_to :user
+  belongs_to :project
+  belongs_to :leader, class_name: 'User'
+  belongs_to :color
+
+  has_many :cards
+  has_many :section_teamings
+  has_many :section_taggings
+
+  # Validations
+  validates :title, presence: true
 end
