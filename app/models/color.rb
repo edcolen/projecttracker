1e65ca73611c7c64d5a290d8cf2aac71238f1d5f
Ed <ed.colen@gmail.com>
2020-10-26 09:45:32 -0300

    updated color model

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,7 +1,7 @@
 class Color < ApplicationRecord
-  validates :name, presence: true
-  validates :hexcode, presence: true
-
   has_many :sections
   has_many :cards
+
+  # Validations
+  validates :name, :hexcode, presence: true
 end
