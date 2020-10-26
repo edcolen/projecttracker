bcb57035c2c39f95e067c6e3ed6eeec26e2768d8
Ed <ed.colen@gmail.com>
2020-10-26 09:57:55 -0300

    updated project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_many :sections
   has_many :cards, through: :sections
   has_many :tags, through: :section_taggings
-  has_many :collaborations, class_name: 'Collaboration', foreign_key: 'project_id'
+  has_many :collaborations
 
   # Validations
   validates :title, presence: true
