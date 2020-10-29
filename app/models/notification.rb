0c08fea375e1a997b8e244317b58909ac38f6c45
Ed <ed.colen@gmail.com>
2020-10-29 09:17:00 -0300

    corrected typo on notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,5 @@ class Notification < ApplicationRecord
   belongs_to :user
 
   # Validations
-  validates :notification_action, notification_target, presence: true
+  validates :notification_action, :notification_target, presence: true
 end
