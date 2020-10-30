91af612669bf5740fb657550fc1175c3a6755a09
Ed <ed.colen@gmail.com>
2020-10-30 10:38:53 -0300

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,21 @@ require 'open-uri'
 require 'faker'
 require 'date'
 
-puts 'Creating users...'
+puts 'Creating main user...'
+sleep(1)
+
+main_user = User.create!(first_name: 'Ed',
+                         last_name: 'Colen',
+                         username: 'edcolen',
+                         email: 'ed.colen@gmail.com',
+                         password: '123456')
+
+# avatar_url = "https://api.adorable.io/avatars/285/#{main_user.id}projecttracker.png"
+# avatar = URI.open(avatar_url)
+# main_user.photo.attach(io: avatar, filename: "#{main_user.username}.png", content_type: 'image/png')
+puts "#{main_user.username} created"
+
+puts 'Creating other users...'
 sleep(1)
 
 10.times do
@@ -17,9 +31,9 @@ sleep(1)
                       email: email,
                       password: password)
 
-  avatar_url = "https://api.adorable.io/avatars/285/#{user.id}projecttracker.png"
-  avatar = URI.open(avatar_url)
-  user.photo.attach(io: avatar, filename: "#{user.username}.png", content_type: 'image/png')
+  # avatar_url = "https://api.adorable.io/avatars/285/#{user.id}projecttracker.png"
+  # avatar = URI.open(avatar_url)
+  # user.photo.attach(io: avatar, filename: "#{user.username}.png", content_type: 'image/png')
   puts "#{user.username} created"
 end
 
@@ -48,14 +62,34 @@ puts 'Creating projects...'
 sleep(1)
 
 3.times do
-  project_user_id = User.all.sample
+  project_user_id = User.all.sample.id
   project_title = Faker::TvShows::Seinfeld.quote
   project_description = Faker::Quote.famous_last_words
-  project_start_date
-  project_end_date
+  project_start_date = Date.today.prev_day(rand(8..20))
+  project_end_date = Date.today.next_day(rand(20..45))
+  Project.create!(user_id: project_user_id,
+                  title: project_title,
+                  description: project_description,
+                  start_date: project_start_date,
+                  end_date: project_end_date)
+  puts "Project #{project.title} created"
 end
 
-puts 'Creating sections...'
+puts 'Creating collaborations...'
+sleep(1)
+
+project_id = Project.all.sample.id
+user_id = project_id.user.id
+member_id = User.all.sample.id
+role = %w[admin developer designer developer designer].sample
+
+collaboration = Collaboration.create!(user_id: user_id,
+                                      member_id: member_id,
+                                      project_id: project_id,
+                                      role: role)
+puts "Assigned #{collaboration.member_id.username} to project: '#{collaboration.project_id.title}'"
+
+puts 'Creating project sections...'
 sleep(1)
 
 puts 'Creating cards...'
