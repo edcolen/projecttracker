e2cf3434c782ba9074d57592b32a9146feb26c46
Ed <ed.colen@gmail.com>
2020-10-30 10:02:33 -0300

    commented user default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  after_create :set_default_avatar
+  # after_create :set_default_avatar
 
   # Relations
   has_many :privileges
@@ -44,11 +44,11 @@ class User < ApplicationRecord
             format: { with: /\A(?=[a-zA-Z0-9._-]{5,20}$)(?!.*[_.]{2})[^_.].*[^_.]\z/,
                       message: 'should contain between 5 and 20 characters' }
 
-  def set_default_avatar
-    return if photo.attached?
+  # def set_default_avatar
+  #   return if photo.attached?
 
-    avatar_url = "https://api.adorable.io/avatars/285/#{id}projecttracker.png"
-    avatar = URI.open(avatar_url)
-    photo.attach(io: avatar, filename: "#{username}.png", content_type: 'image/png')
-  end
+  #   avatar_url = "https://api.adorable.io/avatars/285/#{id}projecttracker.png"
+  #   avatar = URI.open(avatar_url)
+  #   photo.attach(io: avatar, filename: "#{username}.png", content_type: 'image/png')
+  # end
 end
