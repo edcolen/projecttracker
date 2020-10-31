13548b99ab60541bfa2e35dbda019ca619c25cb5
Ed <ed.colen@gmail.com>
2020-10-31 20:53:44 -0300

    updated project model relations

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,9 @@ class Project < ApplicationRecord
   has_many :cards, through: :sections
   has_many :tags, through: :section_taggings
   has_many :collaborations
+  has_many :members, through: :collaborations
   has_many :privileges
+  has_many :admins, through: :privileges
 
   # Validations
   validates :title, presence: true
