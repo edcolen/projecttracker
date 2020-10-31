2e5e8494c06d07591fa47076b2ddf83b633aa092
Ed <ed.colen@gmail.com>
2020-10-31 20:24:21 -0300

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ color_counter = 0
 5.times do
   color_names = %w[red green blue pink gray]
   color_hexcodes = %w[#ff937c #bfff90 #b6f8ff #fbb6ff #d9d9d9]
-  color = Color.create!(name: color_names[counter], hexcode: color_hexcodes[counter])
+  color = Color.create!(name: color_names[color_counter], hexcode: color_hexcodes[color_counter])
   color_counter += 1
   puts "Color #{color.name} created"
 end
@@ -52,7 +52,21 @@ sleep(1)
   puts "Tag #{tag.name} created"
 end
 
-puts 'Creating projects...'
+puts 'Creating main project...'
+sleep(1)
+project_user_id = User.first.id
+project_title = Faker::TvShows::Seinfeld.quote
+project_description = Faker::Quote.famous_last_words
+project_start_date = Date.today.prev_day(rand(8..20))
+project_end_date = Date.today.next_day(rand(20..45))
+project = Project.create!(user_id: project_user_id,
+                          title: project_title,
+                          description: project_description,
+                          start_date: project_start_date,
+                          end_date: project_end_date)
+puts "Project #{project.title} created"
+
+puts 'Creating other projects...'
 sleep(1)
 3.times do
   project_user_id = User.all.sample.id
@@ -60,29 +74,51 @@ sleep(1)
   project_description = Faker::Quote.famous_last_words
   project_start_date = Date.today.prev_day(rand(8..20))
   project_end_date = Date.today.next_day(rand(20..45))
-  Project.create!(user_id: project_user_id,
-                  title: project_title,
-                  description: project_description,
-                  start_date: project_start_date,
-                  end_date: project_end_date)
+  project = Project.create!(user_id: project_user_id,
+                            title: project_title,
+                            description: project_description,
+                            start_date: project_start_date,
+                            end_date: project_end_date)
   puts "Project #{project.title} created"
 end
 
-puts 'Creating privileges...'
+puts 'Creating privileges for main project...'
 sleep(1)
 
-puts 'Creating collaborations...'
-sleep(1)
-project_id = Project.all.sample.id
-user_id = project_id.user.id
-member_id = User.all.sample.id
-role = %w[developer designer UX].sample
+privilege_user_id = User.first.id
+privilege_admin_id = User.first.id
+privilege_project_id = Project.first.id
 
-collaboration = Collaboration.create!(user_id: user_id,
-                                      member_id: member_id,
-                                      project_id: project_id,
-                                      role: role)
-puts "Assigned #{collaboration.member_id.username} to project: '#{collaboration.project_id.title}'"
+privilege = Privilege.create!(user_id: privilege_user_id,
+                              admin_id: privilege_admin_id,
+                              project_id: privilege_project_id)
+puts "#{privilege.user.username} assigned #{privilege.admin.username} to #{privilege.project.title} as admin"
+
+3.times do
+  privilege_user_id = User.first.id
+  privilege_admin_id = User.all.sample.id
+  privilege_project_id = Project.first.id
+
+  privilege = Privilege.create!(user_id: privilege_user_id,
+                                admin_id: privilege_admin_id,
+                                project_id: privilege_project_id)
+  puts "#{privilege.user.username} assigned #{privilege.admin.username} to #{privilege.project.title} as admin"
+end
+
+puts 'Creating collaborations for main project...'
+sleep(1)
+User.all.each do |user|
+  project_id = Project.all.sample.id
+  user_id = User.first.id
+  member_id = user.id
+  role = %w[developer designer UX].sample
+
+  collaboration = Collaboration.create!(user_id: user_id,
+                                        member_id: member_id,
+                                        project_id: project_id,
+                                        role: role)
+  puts "Assigned #{collaboration.member.username} to project: '#{collaboration.project.title}'"
+end
 
 puts 'Creating project sections...'
 sleep(1)
