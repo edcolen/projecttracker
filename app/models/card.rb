d7422e00cfb3b6354ee316618a442dae970f8554
Ed <ed.colen@gmail.com>
2020-11-01 08:52:11 -0300

    updated card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,14 @@
 class Card < ApplicationRecord
   belongs_to :user
-  belongs_to :section
-  belongs_to :project, through: :sections
   belongs_to :leader, class_name: 'User'
+  belongs_to :section
   belongs_to :color
 
   has_many :comments
   has_many :card_taggings
   has_many :card_teamings
+  has_many :members, through: :card_teamings
+  has_many :tags, through: :card_taggings
 
   # Validations
   validates :title, :status, presence: true
