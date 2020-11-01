bc07a24f72898cf62c2e36aa500ec7b0ca1811f7
Ed <ed.colen@gmail.com>
2020-11-01 09:38:16 -0300

    updated card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,13 +5,12 @@ class Card < ApplicationRecord
   belongs_to :color
 
   has_many :comments
-  has_many :members, through: :card_teamings
-  has_many :tags, through: :card_taggings
-  has_many :comment_tags, through: :comment_taggings
-
-  has_many :comment_taggings
-  has_many :card_taggings
   has_many :card_teamings
+  has_many :members, through: :card_teamings
+  has_many :card_taggings
+  has_many :tags, through: :card_taggings
+  has_many :comment_taggings
+  has_many :comment_tags, through: :comment_taggings
 
   # Validations
   validates :title, :status, presence: true
