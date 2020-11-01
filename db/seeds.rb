cb514a6e09e596fbf8d7035d2fabe9eb2c1b8746
Ed <ed.colen@gmail.com>
2020-11-01 08:52:47 -0300

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,8 +58,8 @@ end
 puts 'Creating tags...'
 sleep(1)
 
-15.times do
-  tag_name = Faker::Lorem.word
+20.times do
+  tag_name = Faker::Coffee.variety
   tag = Tag.create!(name: tag_name)
   puts "Tag #{tag.name} created"
 end
@@ -86,8 +86,8 @@ sleep(1)
 
 3.times do
   project_user_id = User.all.sample.id
-  project_title = Faker::TvShows::Seinfeld.quote
-  project_description = Faker::Quote.famous_last_words
+  project_title = Faker::Book.title
+  project_description = Faker::TvShows::Seinfeld.quote
   project_start_date = Date.today.prev_day(rand(8..20))
   project_end_date = Date.today.next_day(rand(20..45))
 
@@ -146,18 +146,18 @@ User.all.each do |user|
   puts "Assigned #{collaboration.member.username} as a #{collaboration.role} to project: '#{collaboration.project.title}'"
 end
 
-puts 'Creating project sections...'
+puts 'Creating main project sections...'
 sleep(1)
 
-4.times do
+5.times do
   section_user_id = Project.first.privileges.sample.user.id
   section_project_id = Project.first.id
   section_color_id = Color.all.sample.id
-  section_leader_id = User.all.sample.id
+  section_leader_id = Project.first.members.sample.id
   section_title = Faker::Book.title
   section_description = Faker::Quote.famous_last_words
-  section_start_date = Date.today.prev_day(rand(8..20))
-  section_end_date = Date.today.next_day(rand(20..45))
+  section_start_date = Project.first.start_date.next_day(rand(8..20))
+  section_end_date = section_start_date.next_day(rand(20..45))
 
   section = Section.create!(
     user_id: section_user_id,
@@ -169,46 +169,147 @@ sleep(1)
     start_date: section_start_date,
     end_date: section_end_date
   )
+
   puts "Section #{section.title} created"
+
+  SectionTeaming.create!(
+    user_id: section_user_id,
+    member_id: section_leader_id,
+    section_id: section.id
+  )
+  puts "Section leader #{section.leader.username} assigned as member"
 end
 
-puts 'Creating sections teamings...'
+puts 'Creating sections teamings for main project...'
 sleep(1)
 
-puts 'Creating cards...'
+Project.first.members.each do |user|
+  2.times do
+    section_teaming_section_id = Section.all.sample.id
+    section_teaming_user_id = Section.find(section_teaming_section_id).leader.id
+    section_teaming_member_id = user.id
+
+    section_teaming = SectionTeaming.create!(
+      user_id: section_teaming_user_id,
+      member_id: section_teaming_member_id,
+      section_id: section_teaming_section_id
+    )
+    puts "Assigned #{section_teaming.member.username} to section: '#{section_teaming.section.title}'"
+  end
+end
+
+puts 'Creating sections taggings for main project...'
 sleep(1)
 
-# 20.times do
-#   card_user_id =
-#     card_section_id
-#   card_leader_id
-#   card_color_id
-#   card_title
-#   card_description
-#   card_start_date
-#   card_end_date
-#   card_priority
-#   card_status
-#   card_category
+Section.all.each do |section|
+  Tag.all.sample(5).each do |tag|
+    section_tagging_user_id = section.members.sample.id
+    section_tagging_section_id = section.id
+    section_tagging_tag_id = tag.id
 
-#   card = Card.create!(
-#     user_id: card_user_id,
-#     section_id: card_section_id,
-#     leader_id: card_leader_id,
-#     color_id: card_color_id,
-#     title: card_title,
-#     description: card_description,
-#     start_date: card_start_date,
-#     end_date: card_end_date,
-#     priority: card_priority,
-#     status: card_status,
-#     category: card_category
-#   )
-#   puts "card '#{card.title}'"
-# end
+    section_tagging = SectionTagging.create!(
+      user_id: section_tagging_user_id,
+      section_id: section_tagging_section_id,
+      tag_id: section_tagging_tag_id
+    )
+    puts "Tagged section '#{section_tagging.section.title}' with #{section_tagging.tag.name}"
+  end
+end
 
-puts 'Creating comments...'
+puts 'Creating main project cards...'
 sleep(1)
+
+25.times do
+  card_section_id = Section.all.sample.id
+  card_user_id = Section.find(card_section_id).members.sample.id
+  card_leader_id = card_user_id
+  card_color_id = Color.all.sample.id
+  card_title = Faker::Book.title
+  card_description = Faker::ChuckNorris.fact
+  card_start_date = Section.find(card_section_id).start_date.next_day(rand(2..20))
+  card_end_date = card_start_date.next_day(rand(20..45))
+  card_priority = %w[low medium high].sample
+  card_status = %w[new new new in\ progress in\ progress done].sample
+  card_category = %w[feature feature issue].sample
+
+  card = Card.create!(
+    user_id: card_user_id,
+    section_id: card_section_id,
+    leader_id: card_leader_id,
+    color_id: card_color_id,
+    title: card_title,
+    description: card_description,
+    start_date: card_start_date,
+    end_date: card_end_date,
+    priority: card_priority,
+    status: card_status,
+    category: card_category
+  )
+  puts "card '#{card.title}' created by #{card.user.username}"
+
+  CardTeaming.create!(
+    user_id: card_user_id,
+    member_id: card_user_id,
+    card_id: card.id
+  )
+  puts "Card leader #{card.leader.username} assigned as member"
+end
+
+puts 'Creating cards teamings for main project...'
+sleep(1)
+
+Project.first.sections.each do |section|
+  section.members.each do |member|
+    rand(1..3).times do
+      card_teaming_card_id = section.cards.sample.id
+      card_teaming_user_id = Card.find(card_teaming_card_id).leader.id
+      card_teaming_member_id = member.id
+
+      card_teaming = CardTeaming.create!(
+        user_id: card_teaming_user_id,
+        member_id: card_teaming_member_id,
+        card_id: card_teaming_card_id
+      )
+      puts "Assigned #{card_teaming.member.username} to card: '#{card_teaming.card.title}'"
+    end
+  end
+end
+
+puts 'Creating card taggings for main project...'
+sleep(1)
+
+Card.all.each do |card|
+  Tag.all.sample(5).each do |tag|
+    card_tagging_user_id = card.members.sample.id
+    card_tagging_card_id = card.id
+    card_tagging_tag_id = tag.id
+
+    card_tagging = CardTagging.create!(
+      user_id: card_tagging_user_id,
+      card_id: card_tagging_card_id,
+      tag_id: card_tagging_tag_id
+    )
+    puts "Tagged card '#{card_tagging.card.title}' with #{card_tagging.tag.name}"
+  end
+end
+
+puts 'Creating mains project comments...'
+sleep(1)
+
+Card.all.each do |card|
+  rand(1..4) do
+    comment_user_id = card.members.sample.id
+    comment_card_id = card.id
+    comment_content = Faker::Quote.famous_last_words
+
+    comment = Comment.create!(
+      user_id: comment_user_id,
+      card_id: comment_card_id,
+      content: comment_content
+    )
+    puts "#{comment.user.username} commented on #{comment.card.title}"
+  end
+end
 
 puts 'Creating responses...'
 sleep(1)
