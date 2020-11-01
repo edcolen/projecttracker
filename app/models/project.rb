91239a7949e0c8a6a1e54b6e57fe66aff339ca92
Ed <ed.colen@gmail.com>
2020-10-31 21:12:24 -0300

    updated project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,6 @@ class Project < ApplicationRecord
 
   has_many :sections
   has_many :cards, through: :sections
-  has_many :tags, through: :section_taggings
   has_many :collaborations
   has_many :members, through: :collaborations
   has_many :privileges
