f69992b3b3ad387261d52667e79d99b381c944d7
Ed Colen <ed.colen@gmail.com>
2020-11-05 20:28:10 -0300

    add project show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,14 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show]
+
+  def show
+    authorize @project
+  end
+
+  private
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
+    redirect_to dashboard_path, notice: 'Project not found.' if @project.nil?
+  end
 end
