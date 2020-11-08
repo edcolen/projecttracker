c3d8f96acb2dacd9c5229b4a4c43ae89b2e79489
Ed Colen <ed.colen@gmail.com>
2020-11-08 10:32:35 -0300

    created section teamings policy

---
+++ app/helpers/section_teamings_helper.rb
@@ -0,0 +1,2 @@
+module SectionTeamingsHelper
+end
---
+++ app/policies/section_teaming_policy.rb
@@ -0,0 +1,15 @@
+class SectionTeamingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    user
+  end
+
+  def create?
+    user
+  end
+end
