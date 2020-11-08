7a761a6cf6d2292d1b2389eacd39786e9c8b28c2
Ed Colen <ed.colen@gmail.com>
2020-11-08 10:12:28 -0300

    added dependent destroy to section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,14 +1,14 @@
 class Section < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  belongs_to :leader, class_name: 'User'
+  belongs_to :leader, class_name: 'User', optional: true
   belongs_to :color
 
-  has_many :cards
-  has_many :section_teamings
-  has_many :members, through: :section_teamings
-  has_many :section_taggings
-  has_many :tags, through: :section_taggings
+  has_many :cards, dependent: :destroy
+  has_many :section_teamings, dependent: :destroy
+  has_many :members, through: :section_teamings, dependent: :destroy
+  has_many :section_taggings, dependent: :destroy
+  has_many :tags, through: :section_taggings, dependent: :destroy
 
   # Validations
   validates :title, presence: true
