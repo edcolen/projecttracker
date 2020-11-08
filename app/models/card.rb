c4f4d6cc11795a0067465b4391d039daf257cedd
Ed Colen <ed.colen@gmail.com>
2020-11-08 10:39:53 -0300

    added dependent destroy to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,13 +4,13 @@ class Card < ApplicationRecord
   belongs_to :section
   belongs_to :color
 
-  has_many :comments
-  has_many :card_teamings
-  has_many :members, through: :card_teamings
-  has_many :card_taggings
-  has_many :tags, through: :card_taggings
-  has_many :comment_taggings
-  has_many :comment_tags, through: :comment_taggings
+  has_many :comments, dependent: :destroy
+  has_many :card_teamings, dependent: :destroy
+  has_many :members, through: :card_teamings, dependent: :destroy
+  has_many :card_taggings, dependent: :destroy
+  has_many :tags, through: :card_taggings, dependent: :destroy
+  has_many :comment_taggings, dependent: :destroy
+  has_many :comment_tags, through: :comment_taggings, dependent: :destroy
 
   # Validations
   validates :title, :status, presence: true
