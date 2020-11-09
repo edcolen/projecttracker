86ad7bed293f95e32f85177a904c2edee69c72b3
Ed Colen <ed.colen@gmail.com>
2020-11-09 14:13:16 -0300

    updated section policy

--- app/policies/section_policy.rb
+++ app/policies/section_policy.rb
@@ -9,28 +9,29 @@ class SectionPolicy < ApplicationPolicy
     record.project.members.include?(user)
   end
 
-  def new?
-    user
-  end
-
-  def create?
-    user
-  end
-
   def edit?
-    privilege_check
+    privilege_check || section_leadership_check
   end
 
   def update?
-    privilege_check
+    privilege_check || section_leadership_check
   end
 
   def destroy?
-    privilege_check
+    privilege_check || section_leadership_check
   end
 
   def privilege_check
     privilege = Privilege.where(admin_id: user.id, project_id: record.project.id)
     privilege.empty? ? false : true
+  end
+
+  def section_membership_check
+    section_membership = SectionTeaming.where(member_id: user.id, section_id: record.id)
+    section_membership.empty? ? false : true
+  end
+
+  def section_leadership_check
+    record.leader.id == user.id
   end
 end
