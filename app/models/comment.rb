02aec4578085e2248d9c476a60cc2e7de17f0297
Ed Colen <ed.colen@gmail.com>
2020-11-14 13:50:13 -0300

    added tags relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :card
 
   has_many :responses
+  has_many :comment_taggings, dependent: :destroy
+  has_many :tags, through: :comment_taggings, dependent: :destroy
 
   # Validations
   validates :content, presence: true
