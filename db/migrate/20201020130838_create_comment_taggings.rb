453974b3e21c37b4a7c235eaee611f6b2c1bb471
Ed Colen <ed.colen@gmail.com>
2020-11-18 14:44:45 -0300

    added card id to comment taggings

--- db/migrate/20201020130838_create_comment_taggings.rb
+++ db/migrate/20201020130838_create_comment_taggings.rb
@@ -1,6 +1,7 @@
 class CreateCommentTaggings < ActiveRecord::Migration[6.0]
   def change
     create_table :comment_taggings do |t|
+      t.references :card, foreign_key: true
       t.references :comment, foreign_key: true
       t.references :tag, foreign_key: true
       t.timestamps
